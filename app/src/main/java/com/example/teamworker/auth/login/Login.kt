99597ada4b9d3fc33adedbec55547be5e736d229
Ivan Ryzhko <ipzk221_rir@student.ztu.edu.ua>
2024-04-30 20:27:22 +0300

    Initital commit. Completed auth algorithm

---
+++ app/src/main/java/com/example/teamworker/auth/login/Login.kt
@@ -0,0 +1,11 @@
+package com.example.teamworker.auth.login
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.teamworker.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
