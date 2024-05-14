4a68b6316d82b7528f1ee9578d195cf8bacf4cce
Ivan Ryzhko <ipzk221_rir@student.ztu.edu.ua>
2024-05-15 02:03:45 +0300

    Повністю робоча авторизація з рівнями доступу

--- app/src/main/java/auth/login/Login.java
+++ app/src/main/java/auth/login/Login.java
@@ -4,27 +4,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.android.volley.Request;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.teamworker.R;
 
-import admin.AdminMain;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 import auth.register.Register;
 import auth.volleyAPI.AuthCallback;
 import auth.volleyAPI.TokenStorageService;
+import admin.AdminMain;
 import auth.volleyAPI.VolleyService;
 import manager.ManagerMain;
 import user.UserMain;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
 
@@ -45,28 +50,22 @@ public class Login extends AppCompatActivity {
         passwordEditText = findViewById(R.id.login_password);
         loginButton = findViewById(R.id.login_loginButton);
         regLink = findViewById(R.id.login_regLink);
-
-
         volleyService = new VolleyService(this);
-
+        tokenStorage = new TokenStorageService(this);
 
         regLink.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-
                 Intent intent = new Intent(Login.this, Register.class);
                 startActivity(intent);
-
             }
         });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 String username = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
-
 
                 JSONObject credentials = new JSONObject();
                 try {
@@ -76,37 +75,65 @@ public class Login extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
-
                 String loginUrl = AUTH_API + "login";
-
 
                 volleyService.login(loginUrl, credentials, new AuthCallback() {
                     @Override
                     public void onSuccess(JSONObject response) {
                         try {
                             String token = response.getString("token");
+                            String username = response.getString("username");
+
                             tokenStorage.saveToken(token);
 
-                            // Отримання даних користувача та збереження їх
-                            JSONObject userJson = response.getJSONObject("user");
-                            tokenStorage.saveUser(userJson.toString());
+                            String userUrl = "http://192.168.0.108:8080/api/v1/users/get/" + username;
+                            JsonObjectRequest userRequest = new JsonObjectRequest(Request.Method.GET, userUrl, null,
+                                    new Response.Listener<JSONObject>() {
+                                        @Override
+                                        public void onResponse(JSONObject userResponse) {
+                                            Log.d("RESPONSE", userResponse.toString());
+                                            JSONArray rolesArray = null;
+                                            String role;
+                                            try {
+                                                rolesArray = userResponse.getJSONArray("roles");
+                                                JSONObject firstRoleObject = rolesArray.getJSONObject(0); // Обрати першу роль з масиву
+                                                role = firstRoleObject.getString("name");
+                                            } catch (JSONException e) {
+                                                throw new RuntimeException(e);
+                                            }
 
-                            // Перевірка ролі користувача
-                            String role = userJson.getString("role_name");
-                            if (!role.isEmpty()) {
-                                if (role.equals("ROLE_ADMIN")) {
-                                    Intent intent = new Intent(Login.this, AdminMain.class);
-                                    startActivity(intent);
-                                } else if (role.equals("ROLE_MANAGER")) {
-                                    Intent intent = new Intent(Login.this, ManagerMain.class);
-                                    startActivity(intent);
-                                } else {
-                                    Intent intent = new Intent(Login.this, UserMain.class);
-                                    startActivity(intent);
+                                            if (!role.isEmpty()) {
+                                                if (role.equals("ROLE_ADMIN")) {
+                                                    Intent intent = new Intent(Login.this, AdminMain.class);
+                                                    startActivity(intent);
+                                                } else if (role.equals("ROLE_MANAGER")) {
+                                                    Intent intent = new Intent(Login.this, ManagerMain.class);
+                                                    startActivity(intent);
+                                                } else {
+                                                    Intent intent = new Intent(Login.this, UserMain.class);
+                                                    startActivity(intent);
+                                                }
+                                            } else {
+                                                Toast.makeText(Login.this, "Role not received", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    },
+                                    new Response.ErrorListener() {
+                                        @Override
+                                        public void onErrorResponse(VolleyError error) {
+                                            Toast.makeText(Login.this, "Error getting user data: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    }) {
+                                @Override
+                                public Map<String, String> getHeaders() {
+                                    Map<String, String> headers = new HashMap<>();
+                                    headers.put("Authorization", "Bearer " + token);
+                                    return headers;
                                 }
-                            } else {
-                                Toast.makeText(Login.this, "Role not received", Toast.LENGTH_SHORT).show();
-                            }
+                            };
+
+                            Volley.newRequestQueue(Login.this).add(userRequest);
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                             Toast.makeText(Login.this, "Error processing server response", Toast.LENGTH_SHORT).show();
@@ -118,8 +145,8 @@ public class Login extends AppCompatActivity {
                         Toast.makeText(Login.this, "Network error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
-
             }
         });
+
     }
 }
--- app/src/main/java/auth/volleyAPI/TokenStorageService.java
+++ app/src/main/java/auth/volleyAPI/TokenStorageService.java
@@ -10,6 +10,8 @@ public class TokenStorageService {
     private static final String TOKEN_KEY = "auth-token";
     private static final String USER_KEY = "auth-user";
     private static final String USER_ROLE = "auth-user-role";
+    private static final String USER_DATA_KEY = "auth-user-data";
+
     private SharedPreferences sharedPreferences;
     private Context context;
     public TokenStorageService(Context context) {
@@ -19,6 +21,7 @@ public class TokenStorageService {
 
     public void saveToken(String token) {
         SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
         editor.putString(TOKEN_KEY, token);
         editor.apply();
     }
@@ -34,6 +37,15 @@ public class TokenStorageService {
         editor.apply();
     }
 
+    public void saveUserData(String userDataJson) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(USER_DATA_KEY, userDataJson);
+        editor.apply();
+    }
+
+    public String getUserData() {
+        return sharedPreferences.getString(USER_DATA_KEY, "");
+    }
 
 
     public User getUser() {
